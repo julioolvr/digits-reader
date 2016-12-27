eb38c6a7fb6a739af5df79b6f3f6d1ce577a54f6
Julio Olivera <julio.olvr@gmail.com>
2016-12-27 11:49:46 -0300

    First steps for optimization

--- cost.jl
+++ cost.jl
@@ -7,3 +7,11 @@ function cost(theta, X, y, lambda)
 
   1 / m * sum(errors) + lambda / 2 / m * sum(theta[2:end] .^ 2)
 end
+
+function gradient(theta, X, y, lambda)
+  m = size(X, 1)
+  predictions = sigmoid(X * theta)
+  lambdas = lambda / m * theta[2:end]
+
+  1 / m * sum((predictions - y) .* X, 1)' + [0;lambdas]
+end
--- loadData.jl
+++ loadData.jl
@@ -2,8 +2,8 @@ module DataLoading
   import GZip
   export gettrainingexamples, gettraininglabels
 
-  TRAINING_IMAGES_URL = "http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz"
-  TRAINING_LABELS_URL = "http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz"
+  const TRAINING_IMAGES_URL = "http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz"
+  const TRAINING_LABELS_URL = "http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz"
 
   function gettrainingexamples()
     readordownloadfile("./data/trainingImages", TRAINING_IMAGES_URL)
